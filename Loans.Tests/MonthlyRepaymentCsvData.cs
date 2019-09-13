d51840ecdf22e7f57e302be73c7fec310c78a40a
rsadat <rsadat@applefcu.org>
2019-09-13 12:17:17 -0400

    Add project files.

---
+++ Loans.Tests/MonthlyRepaymentCsvData.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Loans.Tests
+{
+    public class MonthlyRepaymentCsvData
+    {
+        public static IEnumerable GetTestCases(string csvFileName)
+        {
+            var csvLines = File.ReadAllLines(csvFileName);
+
+            return (from line in csvLines select line.Replace(" ", "").Split(',') into values let principal = decimal.Parse(values[0]) let interestRate = decimal.Parse(values[1]) let termInYears = int.Parse(values[2]) let expectedRepayment = decimal.Parse(values[3]) select new TestCaseData(principal, interestRate, termInYears, expectedRepayment)).ToList();
+        }
+    }
+}
